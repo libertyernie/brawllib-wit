9e4350f30f00e70232348b2197c6fd3a7523bf79
libertyernie <maybeway36@gmail.com>
2021-03-13 16:44:48 -0600

    Implement u8unpack

--- u8unpack/Program.cs
+++ u8unpack/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using BrawlLib.SSBB.ResourceNodes;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace u8unpack {
-    public class Program {
+    public static class Program {
         public const string USAGE = @"u8unpack
 (C) 2021 libertyernie
 https://github.com/libertyernie/brawllib-u8tools
@@ -12,7 +15,44 @@ https://github.com/soopercool101/BrawlCrate
 
 Usage: u8unpack.exe [file.arc]";
 
-        static void Main(string[] args) {
+        public static IEnumerable<char> InvalidCharacters =>
+            Path.GetInvalidFileNameChars()
+            .Concat(Path.GetInvalidPathChars())
+            .Distinct();
+
+        public static void Export(string output_directory, ResourceNode node) {
+            var invalid_file_chars = InvalidCharacters.Intersect(node.Name).ToArray();
+            if (invalid_file_chars.Any())
+                throw new Exception($"The node name {node.Name} contains invalid character(s): {new string(invalid_file_chars)}");
+
+            string output_path = Path.Combine(output_directory, node.Name);
+
+            if (node is U8FolderNode) {
+                foreach (var child in node.Children) {
+                    Directory.CreateDirectory(output_path);
+                    Export(output_path, child);
+                }
+            } else {
+                if (File.Exists(output_path))
+                    throw new Exception($"The file {output_path} already exists.");
+
+                node.Export(output_path);
+            }
+        }
+
+        public static void Unpack(string output_directory, ResourceNode node) {
+            foreach (var c in node.Children) {
+                Export(output_directory, c);
+            }
+        }
+
+        public static void Unpack(string output_directory, string archive_file) {
+            using (var node = NodeFactory.FromFile(null, archive_file)) {
+                Unpack(output_directory, node);
+            }
+        }
+
+        public static void Main(string[] args) {
             if (args.Length != 1 || args[0] == "--help" || args[0] == "/?") {
                 Console.Error.WriteLine(USAGE);
                 return;
@@ -23,7 +63,7 @@ Usage: u8unpack.exe [file.arc]";
                 return;
             }
 
-            throw new NotImplementedException();
+            Unpack(Environment.CurrentDirectory, args[0]);
         }
     }
 }
