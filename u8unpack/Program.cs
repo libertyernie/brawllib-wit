954cea7aa0a0632d9807131b98610b636a5762b8
libertyernie <maybeway36@gmail.com>
2021-03-13 16:08:24 -0600

    Initial commit

---
+++ u8unpack/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace u8unpack {
+    public class Program {
+        public const string USAGE = @"u8unpack
+(C) 2021 libertyernie
+https://github.com/libertyernie/brawllib-u8tools
+
+Built against BrawlLib.dll from BrawlCrate
+https://github.com/soopercool101/BrawlCrate
+
+Usage: u8unpack.exe [file.arc]";
+
+        static void Main(string[] args) {
+            if (args.Length != 1 || args[0] == "--help" || args[0] == "/?") {
+                Console.Error.WriteLine(USAGE);
+                return;
+            }
+
+            if (!File.Exists(args[0])) {
+                Console.Error.WriteLine($"File not found: {args[0]}");
+                return;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
